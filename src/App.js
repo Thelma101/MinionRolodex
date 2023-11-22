8c7d38611febe6a3c5ee74ddd5e4cebc1a4f57f0
Thelma Akpata <50399915+Thelma101@users.noreply.github.com>
2023-11-22 10:55:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from "react";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.components";
 
-function App() {
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [minions, setMinions] = useState([]);
+  const [filteredMinions, setFilteredMinions] = useState(minions);
+
+    useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMinions(users));  
+  }, []);
+
+    useEffect(() => {
+      const newFilteredMinions = minions.filter((minion) => {
+        return minion.name.toLocaleLowerCase().includes(searchField);
+      });
+      setFilteredMinions(newFilteredMinions);
+    }, [minions, searchField]);
+  
+    const onSearchChange = (event) => {
+      const searchFieldString = event.target.value.toLocaleLowerCase();
+      setSearchField(searchFieldString)
+    };
+
   return (
     <div className="App">
       <h1 className="app-title">Minion Rolodex</h1>
